22f0022caf9bf1df0a1651a86a3d6fea9c59cb22
V. SAI DAIWIK <113048796+SaiDaiwikV@users.noreply.github.com>
2024-11-21 00:09:13 +0530

    Add files via upload

---
+++ DAY-3/reverse_an_array.cpp
@@ -0,0 +1,49 @@
+// DONE BY : V SAIDAIWIK;
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here;
+        int n = arr.size();
+        int right = n - 1;
+        int left = 0;
+        while(left < right){
+            swap(arr[left],arr[right]);
+            left++;
+            right--;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
